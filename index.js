79d4aaa07049c5e06d320debb5c3a20a1475183b
andisiambuku <roselandambuku@gmail.com>
2022-08-20 22:56:05 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob";
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(){
+    return bestCustomer="maybe bob";
+}
+
+const leastFavoriteCustomer = "pat";
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer ="not pat";
+}
